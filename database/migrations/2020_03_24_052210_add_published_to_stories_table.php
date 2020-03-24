a876adcfe1c7bd1e31e22808e07f7ff6bc50d518
unknown <shivamsinghal1012@gmail.com>
2020-03-24 11:15:59 +0530

    add published_at in stories

---
+++ database/migrations/2020_03_24_052210_add_published_to_stories_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPublishedToStoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('stories', function (Blueprint $table) {
+             $table->dateTime('published_at');
+        });
+        \DB::statement('UPDATE stories SET published_at = updated_at');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('stories', function (Blueprint $table) {
+            //
+        });
+    }
+}
